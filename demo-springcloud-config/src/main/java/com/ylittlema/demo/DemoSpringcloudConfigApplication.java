9e79434937ff177175d9d80a5c1459ca80da2f44
ylittlema <ylittlema@163.com>
2018-10-17 21:25:36 +0800

    springcloud init

--- demo-springcloud-config/src/main/java/com/ylittlema/demo/DemoSpringcloudConfigApplication.java
+++ demo-springcloud-config/src/main/java/com/ylittlema/demo/DemoSpringcloudConfigApplication.java
@@ -1,4 +1,4 @@
-package com.ylittlema.demo.config;
+package com.ylittlema.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
